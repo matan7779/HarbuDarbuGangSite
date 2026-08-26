1f76eaa390ab66300b53210bd633f39ace183b53
matan7779 <57938401+matan7779@users.noreply.github.com>
2026-08-26 16:13:30 +0300

    Flush cached Aviv special scripts

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="hdg-v14";
+const CACHE="hdg-v15";
 const ASSETS=["./","./index.html","./manifest.webmanifest"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
