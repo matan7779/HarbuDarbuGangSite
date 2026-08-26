ad98d20d2102af0e94afd663c29fa5571f939e01
matan7779 <57938401+matan7779@users.noreply.github.com>
2026-08-26 16:13:53 +0300

    Keep projector on standard trophy

--- config.js
+++ config.js
@@ -1,2 +1,4 @@
 window.HDG_SUPABASE_URL = "https://avxsmtmjwnvfhvymoobd.supabase.co";
 window.HDG_SUPABASE_KEY = "sb_publishable_A3fpigmRqjAS-cjCVvsSWg_uzZJ03bx";
+
+// Aviv special media is intentionally disabled. All winners use the regular trophy.
